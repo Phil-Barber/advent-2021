65c813dd1edc2b1c78f5f1a9cbd787ba2db068d9
Phil Barber <phil.barber93@gmail.com>
2021-12-07 08:58:40 +0000

    day2.1

---
+++ day2/solution.py
@@ -0,0 +1,34 @@
+from collections import namedtuple
+from collections.abc import Iterator
+
+Submarine = namedtuple("Submarine", ("hoz", "depth"))
+
+
+def lines() -> Iterator:
+    with open("input") as input_file:
+        yield from input_file.readlines()
+
+
+def commands(file_lines) -> Iterator:
+    yield from (line.split(" ") for line in file_lines)
+
+
+def move_submarine(submarine, cmd, qty):
+    if cmd == "forward":
+        return Submarine(submarine.hoz + qty, submarine.depth)
+    if cmd == "down":
+        return Submarine(submarine.hoz, submarine.depth + qty)
+    if cmd == "up":
+        return Submarine(submarine.hoz, submarine.depth - qty)
+    raise Exception(f"InvalidCommand: {cmd=}")
+
+
+if __name__ == "__main__":
+    file_lines = lines()
+    submarine = Submarine(0, 0)
+
+    for cmd, qty in commands(file_lines):
+        submarine = move_submarine(submarine, cmd, int(qty))
+
+    answer = submarine.depth * submarine.hoz
+    print(f"Answer: {answer}")
