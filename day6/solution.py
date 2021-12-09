c5fc58f9d2b4f06ea216089da69e88ccc05de70a
Phil Barber <phil.barber93@gmail.com>
2021-12-09 21:01:21 +0000

    day6.2

--- day6/solution.py
+++ day6/solution.py
@@ -1,23 +1,34 @@
+from collections import Counter
+
 CYCLE = 6
 
 
 def tick(ages):
-    new_ages = []
-    for fish_age in ages:
-        if fish_age == 0:
-            new_ages.append(CYCLE)
-            new_ages.append(CYCLE + 2)
+    new_ages = Counter()
+    for age, count in ages.items():
+        if age == 0:
+            new_age = CYCLE
+            new_ages[CYCLE + 2] += count
         else:
-            new_ages.append(fish_age - 1)
+            new_age = age - 1
+        new_ages[new_age] += count
     return new_ages
 
 
 def main(starting_ages, days):
-    if days == 0:
-        return len(starting_ages)
+    day_counter = Counter(starting_ages)
 
-    next_ages = tick(starting_ages)
-    return main(next_ages, days - 1)
+    finished = _main(day_counter, days)
+
+    return sum(finished[elt] for elt in finished)
+
+
+def _main(age_counter, days):
+    if days == 0:
+        return age_counter
+
+    next_ages = tick(age_counter)
+    return _main(next_ages, days - 1)
 
 
 def input_lines() -> list[str]:
@@ -29,5 +40,5 @@ if __name__ == "__main__":
     ages = input_lines()[0].split(",")
     ages[-1] = ages[-1][0]
     ages = [int(age) for age in ages]
-    answer = main(ages, 80)
+    answer = main(ages, 256)
     print(f"Answer: {answer}")
--- day6/test_solution.py
+++ day6/test_solution.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import pytest
 
 import solution as s
@@ -9,16 +11,24 @@ example_ages = [3, 4, 3, 1, 2]
     "days, expected", ((0, len(example_ages)), (18, 26), (80, 5934))
 )
 def test_main(days, expected):
-    assert s.main(example_ages, days) == expected
+    ans = s.main(example_ages, days)
+    assert ans == expected
+
+
+def test__main():
+    end = [6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8]
+    assert s._main(Counter(example_ages), 18) == Counter(end)
 
 
 @pytest.mark.parametrize(
     "ages, expected",
     (
         ([1, 2], [0, 1]),
+        ([0, 1, 1, 2, 3, 2], [6, 0, 0, 1, 2, 1, 8]),
         ([0], [6, 8]),
+        ([0, 0], [6, 6, 8, 8]),
         ([1, 0], [0, 6, 8]),
     ),
 )
 def test_tick(ages, expected):
-    assert s.tick(ages) == expected
+    assert s.tick(Counter(ages)) == Counter(expected)
