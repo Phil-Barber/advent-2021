946d46863c6f2e14b46af69ffa7fd2bfce92f732
Phil Barber <phil.barber93@gmail.com>
2021-12-18 11:40:29 +0000

    day9.2 failed

--- day9/solution.py
+++ day9/solution.py
@@ -1,7 +1,14 @@
+from math import prod
+
+
 def main(lines):
     grid = [list(map(lambda n: int(n), iter(row))) for row in lines]
     low_points = get_low_points(grid)
-    return sum([grid[y][x] + 1 for x, y in low_points])
+    basins = [get_basin(*low_point, grid) for low_point in low_points]
+    basins.sort(key=lambda basin: len(basin), reverse=True)
+    for basin in basins[:5]:
+        print(basin)
+    return prod(len(basin) for basin in basins[:3])
 
 
 def get_low_points(grid):
@@ -34,6 +41,38 @@ def is_lt_down(x, y, grid):
     return y == len(grid) - 1 or grid[y + 1][x] > grid[y][x]
 
 
+def get_basin(x, y, grid, seen=None):
+    if seen is None:
+        seen = set()
+
+    if grid[y][x] == 9:
+        return set()
+
+    seen.add((x, y))
+    basin = {(x, y)}
+
+    coord = (x - 1, y)
+    if x > 0 and is_lt_left(*coord, grid) and coord not in seen:
+        basin |= get_basin(*coord, grid, seen)
+
+    coord = (x + 1, y)
+    if x < len(grid[y]) - 1 and is_lt_right(*coord, grid) and coord not in seen:
+        basin |= get_basin(*coord, grid, seen)
+
+    coord = (x, y - 1)
+    if y > 0 and is_lt_up(*coord, grid) and coord not in seen:
+        basin |= get_basin(*coord, grid, seen)
+
+    coord = (x, y + 1)
+    if y < len(grid) - 1 and is_lt_down(*coord, grid) and coord not in seen:
+        basin |= get_basin(x, y + 1, grid, seen)
+
+    return basin
+
+
+#####
+
+
 def input_lines() -> list[str]:
     with open("input") as input_file:
         return list(input_file.readlines())
@@ -42,6 +81,7 @@ def input_lines() -> list[str]:
 if __name__ == "__main__":
     inputs = input_lines()
     inputs = [line[:-1] for line in inputs if line[0]]
+    print(inputs[0])
     print(inputs[-1])
     answer = main(inputs)
     print(f"Answer: {answer}")
--- day9/test_solution.py
+++ day9/test_solution.py
@@ -14,9 +14,31 @@ grid = [list(map(lambda n: int(n), iter(row))) for row in example]
 
 low_points = [(1, 0), (9, 0), (2, 2), (6, 4)]
 
+basins = [
+    [(1, 0), (0, 0), (0, 1)],
+    [(5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (6, 1), (8, 1), (9, 1), (9, 2)],
+    [
+        (2, 1),
+        (3, 1),
+        (4, 1),
+        (1, 2),
+        (2, 2),
+        (3, 2),
+        (4, 2),
+        (5, 2),
+        (0, 3),
+        (1, 3),
+        (2, 3),
+        (3, 3),
+        (4, 3),
+        (1, 4),
+    ],
+    [(7, 2), (6, 3), (7, 3), (8, 3), (5, 4), (6, 4), (7, 4), (8, 4), (9, 4)],
+]
+
 
 def test_main():
-    assert s.main(example) == 15
+    assert s.main(example) == 1134
 
 
 def test_get_low_points():
@@ -26,3 +48,8 @@ def test_get_low_points():
 @pytest.mark.parametrize("low_point", low_points)
 def test_is_low_point(low_point):
     assert s.is_low_point(*low_point, grid)
+
+
+@pytest.mark.parametrize("low_point, basin", zip(low_points, basins))
+def test_get_basin(low_point, basin):
+    assert set(s.get_basin(*low_point, grid)) == set(basin)
